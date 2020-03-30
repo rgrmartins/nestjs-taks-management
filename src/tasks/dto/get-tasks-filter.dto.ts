8897dbc035c789dcbf6d88715aac90c466aa5096
Roger Martins <roger.martins@omegaenergia.com.br>
2020-03-29 23:17:39 -0300

    feature(nestJS): Continuando os estudos do NestJS e TypeScript

---
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -0,0 +1,12 @@
+import { TaskStatus } from "../tasks.model";
+import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
+
+export class GetTaksFilterDTO {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './tasks.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTaksFilterDTO } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTaksFilterDTO): Task[] {
+    if (Object.keys(filterDto).length){
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDTO): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
